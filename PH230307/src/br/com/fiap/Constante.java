a7df239dd309b6472f2453d5e707af29ffc53d99
matheuscms <matheuschagas415@gmail.com>
2023-03-07 14:06:42 -0300

    Add aula java 070323

---
+++ PH230307/src/br/com/fiap/Constante.java
@@ -0,0 +1,11 @@
+package br.com.fiap;
+
+public class Constante {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = Math.PI;
+		final float GRAVIDADE_TERRSTRE = 9.8f;
+	}
+
+}
---
+++ PH230307/src/br/com/fiap/Primitivo.java
@@ -0,0 +1,19 @@
+package br.com.fiap;
+
+public class Primitivo {
+
+	public static void main(String[] args) {
+		char sexo = 'M';
+		byte idade = 25;
+		short codigo = 15650;
+		float media = 8.7f;
+		int alunos, classes;
+		long brasileiros = 216535748;
+		double dolar = 5.15;
+		boolean alternativa = false;
+		System.out.println("Sexo: " + sexo + "\nIdade: " + idade + "\nCódigo: " + codigo);
+		
+		
+	}
+
+}
